5dafb8370ad5904e323168398d5e0369ba1c025f
sduduzog <gumedesduduzo@gmail.com>
2018-09-28 05:38:59 +0200

    HYperlink on About text

--- app/src/main/java/com/sduduzog/slimlauncher/ui/main/AboutFragment.kt
+++ app/src/main/java/com/sduduzog/slimlauncher/ui/main/AboutFragment.kt
@@ -3,10 +3,12 @@ package com.sduduzog.slimlauncher.ui.main
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sduduzog.slimlauncher.R
+import kotlinx.android.synthetic.main.fragment_about.*
 
 
 class AboutFragment : Fragment() {
@@ -17,5 +19,8 @@ class AboutFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
-
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        aboutDescriptionText.movementMethod = LinkMovementMethod.getInstance()
+    }
 }
