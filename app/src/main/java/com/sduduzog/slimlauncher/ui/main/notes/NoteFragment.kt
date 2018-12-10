39456e1a77dbd853e4f358e696c59cea1c47210e
sduduzog <gumedesduduzo@gmail.com>
2018-12-10 22:36:06 +0200

    Bug fix

--- app/src/main/java/com/sduduzog/slimlauncher/ui/main/notes/NoteFragment.kt
+++ app/src/main/java/com/sduduzog/slimlauncher/ui/main/notes/NoteFragment.kt
@@ -3,7 +3,6 @@ package com.sduduzog.slimlauncher.ui.main.notes
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -84,7 +83,6 @@ class NoteFragment : Fragment(), MainActivity.OnBackPressedListener {
     }
 
     override fun onBackPressed() {
-        Log.d("NoteFragment", "on back pressed")
         saveNote()
     }
 
