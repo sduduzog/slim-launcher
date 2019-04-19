ba4effb6be385c9454a4377da7c3be187fb8602f
beautusg <beautus.gumede@codeo.co.za>
2019-04-19 13:24:50 +0200

    chore: moved a file to another package

--- app/src/main/java/com/sduduzog/slimlauncher/ui/main/NotesFragment.kt
+++ app/src/main/java/com/sduduzog/slimlauncher/ui/main/NotesFragment.kt
@@ -65,6 +65,8 @@ class NotesFragment : BaseFragment(), OnShitDoneToNotesListener {
 
         val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
         itemTouchHelper.attachToRecyclerView(notes_fragment_list)
+
+        notes_fragment_create_note.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_notesFragment_to_noteFragment))
     }
 
 
