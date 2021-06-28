b9840550ca0b420e9e420da71a939df7dba17f6d
Sduu <gumedesduduzo@gmail.com>
2021-06-28 21:45:48 +0200

    :bookmark: Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sduduzog.slimlauncher"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionName = "2.4.19"
-        versionCode = 53
+        versionName = "2.4.20"
+        versionCode = 54
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
         signingConfigs {
