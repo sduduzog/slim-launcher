79ebf1bd0a5e80df62bf941f76dcd4180f8132c4
Sduu <gumedesduduzo@gmail.com>
2021-06-28 16:39:27 +0200

    chore:  lint should not abort on error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,18 @@ android {
         named("release").configure {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             signingConfig = signingConfigs.getByName("release")
         }
         named("debug").configure {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -47,7 +53,9 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
-
+    lintOptions {
+        isAbortOnError = false
+    }
     testOptions {
         unitTests.isIncludeAndroidResources = true
     }
@@ -80,38 +88,4 @@ dependencies {
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha02")
     kapt("androidx.hilt:hilt-compiler:1.0.0-alpha02")
     kapt("com.google.dagger:hilt-android-compiler:2.29-alpha")
-
-    // Test libs
-
-    testImplementation("androidx.test:core:1.3.0")
-
-    testImplementation("androidx.test:runner:1.3.0")
-    testImplementation("androidx.test:rules:1.3.0")
-
-    testImplementation("androidx.test.ext:junit:1.1.2")
-    testImplementation("androidx.test.ext:truth:1.3.0")
-    testImplementation("com.google.truth:truth:1.0")
-
-
-    testImplementation("androidx.arch.core:core-testing:2.1.0")
-    testImplementation("androidx.fragment:fragment-testing:1.3.4")
-
-    testImplementation("org.robolectric:robolectric:4.4")
-
-    testImplementation("org.mockito:mockito-core:2.24.5")
-
-    testImplementation("androidx.test.espresso:espresso-core:3.3.0")
-
-    testImplementation("com.google.dagger:hilt-android-testing:2.29-alpha")
-    kaptTest("com.google.dagger:hilt-android-compiler:2.29-alpha")
-
-    androidTestImplementation("androidx.room:room-testing:2.3.0")
-    androidTestImplementation("androidx.test:runner:1.3.0")
-    androidTestImplementation("androidx.test:rules:1.3.0")
-    androidTestImplementation("androidx.annotation:annotation:1.2.0")
-    androidTestImplementation("androidx.test:runner:1.3.0")
-    androidTestImplementation("androidx.test:rules:1.3.0")
-    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-intents:3.3.0")
 }
